6ad508f3241e46cd1cca65ed1c93e84fe5ed8c40
hex909 <tst.flopp@gmail.com>
2022-07-10 23:25:00 +0530

    countries build

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require( 'express' );
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+// create express application
+const app = express();
+
+// serve static assets
+app.use(express.static( path.resolve( __dirname, '../build' ) ) );
+
+// for any other requests, send `index.html` as a response
+app.use( '*', ( req, res ) => {
+
+    // read `index.html` file
+    let indexHTML = fs.readFileSync( path.resolve( __dirname, '../build/index.html' ), {
+        encoding: 'utf8',
+    } );
+
+    // set header and status
+    res.contentType( 'text/html' );
+    res.status( 200 );
+
+    return res.send( indexHTML );
+} );
+
+// run express server on port 9000
+app.listen( '9000', () => {
+    console.log( 'Express server started at http://localhost:9000' );
+} );
